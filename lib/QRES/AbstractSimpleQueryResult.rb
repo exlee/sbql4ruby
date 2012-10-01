fdfca42f107c806f4bd8aaeebb33ef251052dffb
Marek Zwolinski <marek.zwolinski@gmail.com>
2012-10-01 20:42:54 +0200

    Update

--- lib/QRES/AbstractSimpleQueryResult.rb
+++ lib/QRES/AbstractSimpleQueryResult.rb
@@ -31,6 +31,36 @@ require "lib/QRES/AbstractMethodException"
       return false
     end
     
+    # Dereferences current QRES object searching in SBA store.
+    #
+    # Params:
+    #
+    # var_Store - SBA store where the current object will be dereferenced
+    #
+    # Returns:AbstractSimpleQueryResult
+    #
+    # Throws:IncorrectArgumentException
+    def dereference(var_Store)
+      if(!var_Store.is_a?(SBAStore::SBAStore))
+        raise IncorrectArgumentException("Incorrect argument type [#{var_Store.class.to_s()}], expected [SBAStore]")
+      end
+      
+      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: Executing in context of current object: [#{self.to_s()}], stacks dump:")
+      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: #{@VAR_QRES.to_s()}\n#{@VAR_ENVS.to_s()}")
+      
+      var_SBAObject = nil
+      
+      if(self.VAR_NAME == nil)
+        var_SBAObject = self.to_sba()
+      else
+        var_SBAObject = var_Store.find(self.VAR_OBJECT())
+      end
+       
+      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: Found [#{var_SBAObject.to_s()}]")
+       
+      return var_SBAObject
+    end
+    
     # Abstract method to be implemented by inheriting classes.
     #
     # Params:
@@ -95,6 +125,110 @@ require "lib/QRES/AbstractMethodException"
     def ==(var_RValue)
       raise AbstractMethodException.new("Abstract method")  
     end
+
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def >(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+    
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def >=(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+    
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def <(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def <=(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def %(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+     
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def and(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+    
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def or(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
+        
+    # Abstract method to be implemented by inheriting classes.
+    #
+    # Params:
+    #
+    # var_RValue:AbstractQueryResult - QRES object
+    #
+    # Returns:TrueClass/FalseClass
+    #
+    # Throws:AbstractMethodExcepion    
+    def different(var_RValue)
+      raise AbstractMethodException.new("Abstract method")  
+    end
         
     # Abstract method to be implemented by inheriting classes.
     #
@@ -105,6 +239,6 @@ require "lib/QRES/AbstractMethodException"
     # Throws:AbstractMethodExcepion    
     def to_sba()
       raise AbstractMethodExcepion.new("Abstract method")
-    end  
+    end      
   end
 end
--- lib/QRES/IntegerResult.rb
+++ lib/QRES/IntegerResult.rb
@@ -16,36 +16,6 @@ require "lib/QRES/StringResult"
       
       super(var_Object)
     end
-    
-    # Dereferences current QRES object searching in SBA store.
-    #
-    # Params:
-    #
-    # var_Store - SBA store where the current object will be dereferenced
-    #
-    # Returns:AbstractSimpleQueryResult
-    #
-    # Throws:*IncorrectArgumentException*
-    def dereference(var_Store)
-      if(!var_Store.is_a?(SBAStore::SBAStore))
-        raise IncorrectArgumentException("Incorrect argument type [#{var_Store.class.to_s()}], expected [SBAStore]")
-      end
-      
-      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: Executing in context of current object: [#{self.to_s()}], stacks dump:")
-      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: #{@VAR_QRES.to_s()}\n#{@VAR_ENVS.to_s()}")
-      
-      var_SBAObject = nil
-      
-      if(self.VAR_NAME == nil)
-        var_SBAObject = self.to_sba()
-      else
-        var_SBAObject = var_Store.find(self.VAR_OBJECT())
-      end
-       
-      Common::Logger.print(Common::VAR_DEBUG, self, "[dereference]: Found [#{var_SBAObject.to_s()}]")
-       
-      return var_SBAObject
-    end
 
     # Overloaded operator adds the IntegerResult encapsulated values
     # and returns a new AbstractSimpleQueryResult object which provides 
