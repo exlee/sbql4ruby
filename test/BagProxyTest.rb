c79a35eabaae905213816226a474eb35758fb9df
Marek Zwolinski <marek.zwolinski@gmail.com>
2012-10-06 21:15:56 +0200

    Comma tests

---
+++ test/BagProxyTest.rb
@@ -0,0 +1,130 @@
+module BagProxyTest
+
+require "test/unit"
+require "lib/Common/Logger"
+
+Dir["lib/QRES/*.rb"].each {|file| require file }
+Dir["lib/Operator/*.rb"].each {|file| require file }
+Dir["lib/AST/*.rb"].each {|file| require file }
+
+
+    class ParserLexerTest < Test::Unit::TestCase
+        include AST
+        include QRES
+        def setup
+          Common::Logger.setLogLevel(Common::VAR_INFO)
+          @AST = AST.new("sampledata/data.xml")
+          
+        end
+        
+        def execute(expression)
+          expression.execute(@AST)
+          @result = @AST.VAR_QRES().pop()
+        end
+        
+        def test_bagProxyNoConversion
+          bagResult = BagResult.new()
+          
+          bagResult.push(IntegerResult.new(1))
+          bagResult.push(IntegerResult.new(2))
+          bagResult.push(IntegerResult.new(3))
+          
+          assert(bagResult.equals(Utils::bagResultProxy(bagResult)))
+        end
+        
+        def test_bagProxyNoConversionWithStruct
+          bagResult       = BagResult.new()
+          structResult    = StructResult.new()
+          extectedResult  = BagResult.new()
+
+          structResult.push(IntegerResult.new(1))
+          structResult.push(IntegerResult.new(2))
+          structResult.push(IntegerResult.new(3))
+          
+          bagResult.push(structResult)
+          bagResult.push(structResult)
+
+          assert(bagResult.equals(Utils::bagResultProxy(bagResult)))
+        end
+
+        def test_bagProxyConversion
+          bagResult       = BagResult.new()
+          structResult    = StructResult.new()
+          extectedResult  = BagResult.new()
+          
+          structResult.push(IntegerResult.new(1))
+          structResult.push(IntegerResult.new(2))
+          structResult.push(IntegerResult.new(3))
+          
+          bagResult.push(structResult)
+             
+          # Expected result     
+          extectedResult = BagResult.new()
+          
+          extectedResult.push(IntegerResult.new(1))
+          extectedResult.push(IntegerResult.new(2))
+          extectedResult.push(IntegerResult.new(3))
+          
+          assert(extectedResult.equals(Utils::bagResultProxy(bagResult)))
+        end        
+       
+        def test_bagProxyConversionPop
+          bagResult       = BagResult.new()
+          structResult    = StructResult.new()
+          extectedResult  = BagResult.new()
+          
+          structResult.push(IntegerResult.new(1))
+          structResult.push(IntegerResult.new(2))
+          structResult.push(IntegerResult.new(3))
+          
+          bagResult.push(structResult)
+          
+          bagResult.pop()
+          
+          # Expected result     
+          extectedResult = BagResult.new()
+          
+          extectedResult.push(IntegerResult.new(1))
+          extectedResult.push(IntegerResult.new(2))
+           
+          assert(extectedResult.equals(Utils::bagResultProxy(bagResult)))
+          
+          bagResult.pop()
+          extectedResult.pop()
+         
+          assert(extectedResult.equals(Utils::bagResultProxy(bagResult))) 
+        end
+        
+        def test_bagProxyIsSimpleResult
+          bagResult       = BagResult.new()
+          structResult    = StructResult.new()
+          
+          structResult.push(IntegerResult.new(1))
+          structResult.push(IntegerResult.new(2))
+          structResult.push(IntegerResult.new(3))
+          
+          bagResult.push(structResult)
+
+          assert_equal(false, bagResult.isSimpleObject?())
+          
+          bagResult.pop()
+          bagResult.pop()
+          
+          assert_equal(true, bagResult.isSimpleObject?())
+        end
+
+        def test_bagProxyGetAsSimpleResult
+          bagResult       = BagResult.new()
+          structResult    = StructResult.new()
+          
+          structResult.push(IntegerResult.new(1))
+          
+          bagResult.push(structResult)
+
+          # Expected result     
+          extectedResult = IntegerResult.new(1)
+          
+          assert(extectedResult.equals(bagResult.getAsSimpleResult()))           
+        end
+    end
+end
---
+++ test/CommaFixTest.rb
@@ -0,0 +1,187 @@
+module CommaFixTest
+
+require "test/unit"
+require "lib/Common/Logger"
+
+Dir["lib/QRES/*.rb"].each {|file| require file }
+Dir["lib/Operator/*.rb"].each {|file| require file }
+Dir["lib/AST/*.rb"].each {|file| require file }
+
+
+    class ParserLexerTest < Test::Unit::TestCase
+        include AST
+        include QRES
+        def setup
+          Common::Logger.setLogLevel(Common::VAR_DEBUG)
+          @AST = AST.new("sampledata/data.xml")
+          
+        end
+        
+        def execute(expression)
+          expression.execute(@AST)
+          @result = @AST.VAR_QRES().pop()
+        end
+        
+        def test_SimpleSimple
+          expression = CommaExpression.new(IntegerTerminal.new(1),IntegerTerminal.new(2))
+
+          self.execute(expression)
+          
+          expected = BagResult.new()
+          expected.push(IntegerResult.new(1))
+          expected.push(IntegerResult.new(2))
+  
+          # 1,2 = b(1,2)
+          assert(expected.equals(Utils::bagResultProxy(@result)), "1,2 -> BagResult(1,2)")
+        end
+        
+        def test_SimpleBag
+          expression = CommaExpression.new(
+            IntegerTerminal.new(1),
+            CommaExpression.new(
+              IntegerTerminal.new(2),
+              IntegerTerminal.new(3)
+            )
+          )
+
+          self.execute(expression)
+
+          expected = BagResult.new()
+          expected.push(IntegerResult.new(1))
+          expected.push(IntegerResult.new(2))
+          expected.push(IntegerResult.new(3))
+
+          # 1,b(2,3) = b(1,2,3)
+          assert(expected.equals(Utils::bagResultProxy(@result)), "1,b(2,3) -> BagResult(1,2,3)")
+         end
+         
+        def test_BagSimple
+          expression = CommaExpression.new(
+            CommaExpression.new(
+              IntegerTerminal.new(1),
+              IntegerTerminal.new(2)
+            ),
+            IntegerTerminal.new(3)
+          )
+
+          self.execute(expression)
+
+          expected = BagResult.new()
+          expected.push(IntegerResult.new(1))
+          expected.push(IntegerResult.new(2))
+          expected.push(IntegerResult.new(3))
+
+          # b(1,2),3 = b(1,2,3)
+          assert(expected.equals(Utils::bagResultProxy(@result)), "b(1,2),3-> BagResult(1,2,3)")
+        end
+        
+        def test_BagBag
+          expression = CommaExpression.new(
+            CommaExpression.new(
+              IntegerTerminal.new(1),
+              IntegerTerminal.new(2)
+            ),
+            CommaExpression.new(
+              IntegerTerminal.new(3),
+              IntegerTerminal.new(4)
+            )
+          )
+
+          self.execute(expression)
+
+          expected = BagResult.new()
+          
+          struct1 = StructResult.new()
+          struct1.push(IntegerResult.new(1))
+          struct1.push(IntegerResult.new(3))
+          
+          struct2 = StructResult.new()
+          struct2.push(IntegerResult.new(1))
+          struct2.push(IntegerResult.new(4))
+
+          struct3 = StructResult.new()
+          struct3.push(IntegerResult.new(2))
+          struct3.push(IntegerResult.new(3))
+
+          struct4 = StructResult.new()
+          struct4.push(IntegerResult.new(2))
+          struct4.push(IntegerResult.new(4))
+
+          expected.push(struct1)
+          expected.push(struct2)
+          expected.push(struct3)
+          expected.push(struct4)
+          
+          # b(1,2), b(3,4) = b(s(1,3),s(1,4),s(2,3),s(2,4))
+          assert(expected.equals(@result), "b(1,2), b(3,4) -> BagResult(StructResult(1,3),StructResult(1,4),StructResult(2,3),StructResult(2,4))")
+        end
+    
+        def test_BagStruct
+          expression = CommaExpression.new(
+            CommaExpression.new(
+              IntegerTerminal.new(1),
+              IntegerTerminal.new(2)
+            ),
+            StructExpression.new(CommaExpression.new(IntegerTerminal.new(3), IntegerTerminal.new(4)))
+          )
+
+          #expression = StructExpression.new(CommaExpression.new(IntegerTerminal.new(1), IntegerTerminal.new(2)))
+          self.execute(expression)
+
+          expected = BagResult.new()
+          
+          struct1 = StructResult.new()
+          struct1.push(IntegerResult.new(1))
+          struct1.push(IntegerResult.new(2))
+          
+          struct2 = StructResult.new()
+          struct2.push(IntegerResult.new(3))
+          struct2.push(IntegerResult.new(4))
+
+          expected.push(struct1)
+          expected.push(struct2)
+          
+          # b(1,2), s(3,4) = b(s(1,2),s(3,4))
+          assert(expected.equals(@result), "b(1,2), s(3,4) = b(s(1,2),s(3,4))")
+        end
+
+        def test_StructSimlpe
+          expression = CommaExpression.new(
+            StructExpression.new(CommaExpression.new(IntegerTerminal.new(1), IntegerTerminal.new(2))),
+            IntegerTerminal.new(3)
+          )
+
+          #expression = StructExpression.new(CommaExpression.new(IntegerTerminal.new(1), IntegerTerminal.new(2)))
+          self.execute(expression)
+
+          expected = BagResult.new()
+          expected.push(IntegerResult.new(1))
+          expected.push(IntegerResult.new(2))
+          expected.push(IntegerResult.new(3))
+          
+          # s(1,2),3 = b(1,2,3)
+          assert(expected.equals(Utils::bagResultProxy(@result)), "s(1,2),3 -> BagResult(1,2,3)")
+        end
+        
+        def test_StructStruct
+          expression = CommaExpression.new(
+            StructExpression.new(CommaExpression.new(IntegerTerminal.new(1), IntegerTerminal.new(2))),
+            StructExpression.new(CommaExpression.new(IntegerTerminal.new(3), IntegerTerminal.new(4)))
+          )
+
+          #expression = StructExpression.new(CommaExpression.new(IntegerTerminal.new(1), IntegerTerminal.new(2)))
+          self.execute(expression)
+
+          #expected = StructResult.new()
+          #expected.push(IntegerResult.new(1))
+          #expected.push(IntegerResult.new(2))
+          #expected.push(IntegerResult.new(3))
+          
+          # s(1,2),s(3,4) = b(s(1,2,3,4))
+          
+          puts "AAA RES Final #{@result.to_s()} -> #{(Utils::bagResultProxy(@result)).to_s()} / expected=#{expected.to_s()}"
+          
+          assert(expected.equals(Utils::bagResultProxy(@result)), "s(1,2),3 -> StructResult(1,2,3)")
+        end
+    end
+end
